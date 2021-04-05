3505fc3cb895eb846be84f2e4e9fc046355f5abb
rage997 <zuppin@usi.ch>
2021-04-05 09:26:34 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prey Preadator
+
+Agent model simulation of a prey predatol system. The settings can be changed in '''settings.py'''
---
+++ settings.py
@@ -0,0 +1,19 @@
+# Domain size
+L = 10
+# Time of the simulation
+T = 1000
+
+# Number of rabbits, wolves
+Nr, Nw = 900, 100
+
+sigma = 0.5 # sigma for step length parameter
+
+# probability to replicate
+PR_r = PR_w =  0.02
+
+# Die age (rabbit) or hunger (for wolves)
+TR_d, TW_d = 100, 50
+
+# Eat
+r_c = 0.5 # radius
+PW_e = 0.02 # probability to eat
